4d3c0e286f6691526fc89266873a13403bb036ee
nikitooooos <nikita.postnikov.00@gmail.com>
2024-01-11 16:09:00 +0300

    delite attach

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,7 +2,6 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.AfterEach;
@@ -19,27 +18,5 @@ public class TestBase {
     static void beforeAllCase() {
         RestAssured.baseURI = "https://reqres.in";
         RestAssured.basePath = "/api";
-    }
-    @BeforeEach
-    public void beforeEach() {
-        Configuration.remote = System.getProperty("remote", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
-
-        SelenideLogger.addListener("allure", new AllureSelenide());
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
-        Configuration.browserCapabilities = capabilities;
-    }
-
-    @AfterEach
-    void addAttachments() {
-        Attach.screenshotAs("Last screenshot");
-        Attach.browserConsoleLogs();
-        Attach.pageSource();
-        Attach.addVideo();
-        closeWebDriver();
     }
 }
